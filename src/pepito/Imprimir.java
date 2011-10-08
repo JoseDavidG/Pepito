9840a46173bb951616bb80430b125515c8f72a0d
Jose <jose_david1991@hotmail.com>
2011-10-07 20:21:58 -0600

    coment

---
+++ src/pepito/Imprimir.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pepito;
+
+/**
+ *
+ * @author David
+ */
+public class Imprimir {
+    public static void main(String args[]){
+        System.out.println("Jose Gomez");
+    }
+    
+}
